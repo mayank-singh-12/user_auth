c4e533e9258ed0ef362b89f9f0cd7ab04698b860
mayank-singh-12 <dev.by.mayank@gmail.com>
2025-07-26 12:22:44 +0530

    Added essential tables

---
+++ parking_app/application/controller.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Blueprint
+from application.model import User
+
+root = Blueprint("root", __name__, template_folder="templates/root")
+
+@root.route("/",methods=["GET","POST"])
+def dashboard():
+    if request.method == "GET":
+        if "user_info" not in session:
+            return render_template("login.html")
+
